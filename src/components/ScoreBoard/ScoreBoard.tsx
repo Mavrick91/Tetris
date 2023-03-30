0a47b166782457551d871cf71737fe785089fecc
Shady <clownclowed@gmail.com>
2023-03-30 17:09:00 +0800

    👀 Update background

--- src/components/ScoreBoard/ScoreBoard.tsx
+++ src/components/ScoreBoard/ScoreBoard.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ScoreBoard: FC<Props> = ({ score }) => {
   return (
-    <div className="flex flex-col items-center absolute -top-[76px] left-0 right-0 font-sans text-white">
+    <div className="flex flex-col items-center absolute -top-[76px] inset-x-0 font-sans text-white">
       <div className="text-xl font-bold mb-2">Score</div>
       <div className="text-2xl font-mono">{score.toLocaleString()}</div>
     </div>
